64a4eca3fc02eee9b53ac6e2cebf257536f1f2c5
pg686 <83918472+pg686@users.noreply.github.com>
2024-06-17 20:05:12 +0300

    refactor PizzaCreate component

--- src/components/PizzaCreate/PizzaCreate.jsx
+++ src/components/PizzaCreate/PizzaCreate.jsx
@@ -2,75 +2,69 @@ import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from "react";
 import * as pizzaService from '../../services/pizzaService.js';
 import { useParams } from 'react-router-dom';
+
 export default function PizzaCreate() {
     const navigate = useNavigate();
     let { pizzaId } = useParams();
-const [products, setProducts] = useState({});
-const [description, setDescription] = useState([]);
-const [pizzaType, setPizzaType] = useState('vegan');
-const [price, setPrice] = useState(0);
+
+    const [products, setProducts] = useState({});
+    const [description, setDescription] = useState([]);
+    const [pizzaType, setPizzaType] = useState('vegan');
+    const [price, setPrice] = useState(0);
+
     useEffect(() => {
-if(!pizzaId){
-     pizzaService.getAllIngredients().then(result => {
-
-        const filteredProducts = Object.entries(result[0]).filter(x => x[0] !== '_id');
-        setProducts(Object.fromEntries(filteredProducts));
-       })
-}
-      
-
-   }
-   , [] );
-
-   const onProductChange = (e) => {
-    const category = Object.entries(products).find(x => Object.keys(x[1]).includes(e.target.value))[0];
-    const productsCopy = {...products,
-        [category]: {
-            ...products[category],
-            [e.target.value]: { ...products[category][e.target.value],
-                "checked": e.target.checked
-              
-            } 
+        if (!pizzaId) {
+            pizzaService.getAllIngredients().then(result => {
+                const filteredProducts = Object.entries(result[0]).filter(([key]) => key !== '_id');
+                setProducts(Object.fromEntries(filteredProducts));
+            });
         }
-        
+    }, [pizzaId]);
+
+    const onProductChange = (e) => {
+        const { value, checked } = e.target;
+
+        // Find the category and ingredient
+        const category = Object.keys(products).find(cat => value in products[cat]);
+        if (!category) {
+            console.error('Product not found in any category');
+            return;
+        }
+
+        const ingredient = products[category][value];
+        const ingredientPrice = ingredient.price;
+
+        // Update products state
+        const updatedProducts = {
+            ...products,
+            [category]: {
+                ...products[category],
+                [value]: { ...ingredient, checked }
+            }
         };
-    setProducts(productsCopy);
-    const ingredientArr = Object.entries(productsCopy[category]).find(x =>  x[0] === e.target.value)
 
+        setProducts(updatedProducts);
 
+        // Update price and description
+        setPrice(prevPrice => checked ? prevPrice + ingredientPrice : prevPrice - ingredientPrice);
+        setDescription(prevDescription => checked
+            ? [...prevDescription, value]
+            : prevDescription.filter(item => item !== value)
+        );
 
+        // Update pizza type
+        const allCheckedIngredients = Object.values(updatedProducts).flatMap(cat =>
+            Object.values(cat).filter(item => item.checked)
+        );
 
-
-console.log(ingredientArr[1].checked,"ingredientArr[1].checked")
-if(ingredientArr[1].checked){
-    setPrice(price => price + ingredientArr[1].price);
-   
-    if(!description.includes(ingredientArr[0])){
-        setDescription(x => [...x, ingredientArr[0]]);
-    }
-    
-}else{
-    setPrice(price => price - ingredientArr[1].price);
-    if(description.includes( ingredientArr[0])){
-        setDescription(x => x.filter(x => x !==  ingredientArr[0]));
-    }
-}
-
-const checkedArr = Object.values(productsCopy).reduce((acc, curr) => {
-    const checked = Object.values(curr)?.filter(x => x.checked === true).map(x => x.type);
-acc.push(...checked);
-return acc;
-}, []);
-if(checkedArr.includes("meat")){
-    setPizzaType("meat")
-}else if(checkedArr.includes("vegetarian")){
-    setPizzaType("vegetarian")
-}else{
-    setPizzaType("vegan")
-}
-   }
-console.log(description, "description");
-console.log(price, "price");
+        if (allCheckedIngredients.some(item => item.type === 'meat')) {
+            setPizzaType('meat');
+        } else if (allCheckedIngredients.some(item => item.type === 'vegetarian')) {
+            setPizzaType('vegetarian');
+        } else {
+            setPizzaType('vegan');
+        }
+    };
 
     return (
         <section id="create-page" className="auth">
@@ -78,48 +72,47 @@ console.log(price, "price");
                 <div className="container">
                     <h1>Create Pizza</h1>
                     <div className='input-box'>
-                    <label htmlFor="leg-title">Legendary title:</label>
-                    <input type="text" id="title" name="title" placeholder="Enter pizza title..." />
+                        <label htmlFor="pizza-name">Pizza name:</label>
+                        <input type="text" id="name" name="name" placeholder="Enter pizza name..." />
                     </div>
                     <div className='input-box'>
-                    <label htmlFor="pizza-img">Image:</label>
-                    <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
+                        <label htmlFor="pizza-img">Image:</label>
+                        <input type="text" id="imageUrl" name="imageUrl" placeholder="Upload a photo..." />
                     </div>
                     <div className='input-box'>
-            <label htmlFor="description">Description</label>
-            <p>{description.join(", ") || ''}</p>
-            </div>
-            <div className='input-box'>
-            <label htmlFor="price">Price</label>
-            <p>{price}</p>
-            </div>
-            <div className='input-box'>
-            <label htmlFor="type">Type</label>
-            <p>{pizzaType}</p>
-            </div>
+                        <label htmlFor="description">Description</label>
+                        <p>{description.join(", ") || ''}</p>
+                    </div>
+                    <div className='input-box'>
+                        <label htmlFor="price">Price</label>
+                        <p>{price}</p>
+                    </div>
+                    <div className='input-box'>
+                        <label htmlFor="type">Type</label>
+                        <p>{pizzaType}</p>
+                    </div>
                     <div className="ingredients">
-                     { Object.keys(products).map(category => {
-                       
-return (
-<div className="ingredientsContainer" key={category}>
-    <h3>{category}</h3>
-    { Object.keys(products[category]).map(ingredient => {
-  return ( <div className="ingredient" key={ingredient}>
-    <p>{ingredient}</p>
-<label>
-<input type="checkbox" id="title" name="title" value={ingredient} onChange={ onProductChange} checked={products[category][ingredient].checked}></input>
-<span></span>
-</label>
-</div>)
-    })
-
- }
-</div>)
-                     })
-
-             }
-                          </div>
-                          <input className="btn submit" type="submit" value="Create Pizza" />
+                        {Object.keys(products).map(category => (
+                            <div className="ingredientsContainer" key={category}>
+                                <h3>{category}</h3>
+                                {Object.keys(products[category]).map(ingredient => (
+                                    <div className="ingredient" key={ingredient}>
+                                        <p>{ingredient}</p>
+                                        <label>
+                                            <input
+                                                type="checkbox"
+                                                value={ingredient}
+                                                onChange={onProductChange}
+                                                checked={products[category][ingredient].checked || false}
+                                            />
+                                            <span></span>
+                                        </label>
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                    </div>
+                    <input className="btn submit" type="submit" value="Create Pizza" />
                 </div>
             </form>
         </section>
